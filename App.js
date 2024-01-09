f0560b1e9be4d2cf8906fddababa42b299aa9409
jaswanth-Gadde <141471086+jaswanth-Gadde@users.noreply.github.com>
2024-01-09 20:20:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d15315.907042619037!2d80.41816749359755!3d16.324131650283743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sRestaurants!5e0!3m2!1sen!2sin!4v1704717007286!5m2!1sen!2sin" width="1550" height="700" style={{ border: "0" }}  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  );
+}
+
+export default App;
